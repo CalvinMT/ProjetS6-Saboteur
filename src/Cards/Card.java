806873aae491d8a07186786006b3472ce2ee5531
Gaetan Sorin <gaetan.sorin@gmail.com>
2017-05-03 10:50:07 +0200

    Ajout class Card + tests

---
+++ src/Cards/Card.java
@@ -0,0 +1,25 @@
+package Cards;
+
+public class Card {
+    private enum Type {action, player, gallery};
+    private final Type type;
+    private int x, y;
+
+    Card(type t, int x, int y) {
+        this.type = t;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
