6288081c2c02607196ad8e6deec34a6a49c3e8d8
Gaetan Sorin <gaetan.sorin@gmail.com>
2017-05-03 11:26:46 +0200

    Correction Card.java
    Suppression des coordonnees dans toutes les cartes, deplacement dans GalleryCard.java

--- src/Cards/Card.java
+++ src/Cards/Card.java
@@ -1,25 +1,17 @@
 package Cards;
 
 public abstract class Card {
-    private enum Type {action, player, gallery};
+    public enum Type {action, player, gallery};
     private final Type type;
-    private int x, y;
 
-    Card(type t, int x, int y) {
+    Card() {
+        this.type = Type.player;
+    }
+    Card(Type t) {
         this.type = t;
-        this.x = x;
-        this.y = y;
     }
 
     public Type getType() {
         return type;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 }
--- test/Cards/CardTest.java
+++ test/Cards/CardTest.java
@@ -6,25 +6,11 @@ import org.junit.jupiter.api.Test;
 class CardTest {
     @Test
     void getType() {
-        Card cAction = new Card(action, 3, 3);
-        Card cPlayer = new Card(player, 3, 3);
-        Card cGallery = new Card(gallery, 3, 3);
+        Card cAction = new Card(Card.Type.action, 3, 3);
+        Card cPlayer = new Card(Card.Type.player, 3, 3);
+        Card cGallery = new Card(Card.Type.gallery, 3, 3);
         Assertions.assertTrue(cAction.getType() = Card.Type.action);
         Assertions.assertTrue(cPlayer.getType() = Card.Type.player);
         Assertions.assertTrue(cGallery.getType() = Card.Type.gallery);
     }
-
-    @Test
-    int getX() {
-        Card c = new Card(action, 1, 1);
-        Assertions.assertTrue(c.getX() = 1);
-    }
-    @Test
-    int getY() {
-        Card c = new Card(action, 1, 1);
-        Assertions.assertTrue(c.getY() = 1);
-    }
-
-
-
 }
