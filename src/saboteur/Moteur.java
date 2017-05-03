7bb33c630182b072f1c9188e3ac896c9d516df81
e-mez <emez.uwalaka@gmail.com>
2017-05-03 15:37:19 +0200

    Autres methodes ajoutés dans la classe mais pas encore implementés

--- src/saboteur/Moteur.java
+++ src/saboteur/Moteur.java
@@ -11,42 +11,84 @@ import Cards.*;
  * @author uwalakae
  */
 public class Moteur {
-    // attributs
+    // attributs (autres à ajouter par la suite)
+	Player [] players;
     
     // constructeur
     
     
+	// verifie que cardToPlay peut prolonger la gallerie en respectant les regles si mis adjacent 
+	// à cardToAppendTo
+	public boolean verifyAppendToGallery(Player player, Card cardToPlay, Card cardToAppendTo) {
+		return false;   
+    }
+	
+	// player.playableCards sera modifié du coup; la gallerie aussi
+    public void appendToGallery(Player player, Card cardToPlay, Card cardToAppendTo) {
+        // à implementer
+    	if (isGameOver()) {
+    		
+    	}
+    }
     
-    // ajoute une carte qui va amener au "trésor potentiel" sur la gallerie
-    public void towardsTreasure(Joueur player, Card cardToPlay, Card cardToAppend) {
+    
+    // verification que cardToPlay peut detruire un cul de sac, puis que x & y sont des indices 
+    // valides de la gallerie et que (x,y) contient une carte de type cul de sac
+    public boolean verifyDestroyCulDeSac(Player player, Card cardToPlay, int x, int y) {
+    	return false;
+    }
+    
+    // enleve une carte de type cul de sac, modifie la gallerie, et 
+    // modifie le tableau player.playableCards 
+    public void destroyCulDeSac(Player player, Card cardToPlay, int x, int y) {
         
     }
     
-    // enleve une carte qui empeche d'arriver au(x) but(s)
-    // serviable pour un joueur qui est un miner
-    public void destroyCulDeSac(Joueur player, Card cardToPlay) {
+    //verifie le type de cardToPlay et que ça pourrait "soigner" pausedPlayer (si pausedPlayer a besoin)
+    public boolean verifyUnpausePlayer(Player player, Card cardToPlay, Player pausedPlayer) {
+    	return false;
+    }
+    
+    // enleve une carte de pause correspondant au type de Carte c devant le Player j
+    // pourrait l'utiliser pour lui meme
+    // modifie le tableau player.playableCards et pausedPlayer.pauseCards
+    public void unpausePlayer(Player player, Card cardToPlay, Player pausedPlayer) {
         
     }
     
-    // enleve une carte de pause correspondant au type de Carte c devant le Joueur j
-    // pourrait meme utiliser pour lui meme
-    public void healPlayer(Joueur player, Card cardToPlay, Joueur woundedPlayer) {
+    
+    public boolean verifyPausePlayer(Player player, Card cardToPlay, Player targetedPlayer) {
+    	return false;
+    }
+    
+    public void pausePlayer(Player player, Card cardToPlay, Player targetedPlayer) {
         
     }
     
-    public void sabotagePlayer(Joueur player, Card cardToPlay, Joueur targetedPlayer) {
+    // verifie que player possede une carte de type Map,
+    // verifie (x,y), puis lui renvoie le type de la carte sur la case (x,y). 
+    // player.playableCards sera modifié et aussi player.treasureCardsChecked 
+    public boolean verifyPeep(Player player, Card cardToPlay, Card possibleTreasure) {
+    	return false;
+    }
+    
+    public void peepCard(Player player, Card cardToPlay) {
+        
+    }
+       
+    public boolean isGameOver() {
+    	return false;
+    }
+    
+    // si les mineurs gagnent : 
+    public void shareGoldAmongMiners(Player [] players){
         
     }
     
-    public void peepCard(Joueur player, Card cardToPlay) {
+ // si les saboteurs gagnent : 
+    public void shareGoldAmongSaboteurs(Player [] players){
         
     }
     
-    public void shareGoldAmongMiners(Joueur [] players){
-        
-    }
-    
-    public void shareGoldAmongSaboteurs(Joueur [] players){
-        
-    }
+
 }
