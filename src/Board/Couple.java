ed65369c0428fadfcddda98e8ba9b353292eb8be
Gaetan Sorin <gaetan.sorin@gmail.com>
2017-05-09 15:26:58 +0200

    Ajout classe Couple + tests

---
+++ src/Board/Couple.java
@@ -0,0 +1,24 @@
+package Board;
+
+public class Couple {
+    private int x, y;
+
+
+    Couple() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    Couple(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
