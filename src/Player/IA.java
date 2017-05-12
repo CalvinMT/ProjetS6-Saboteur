21525f63528d7db65c5d08be640efe04c5263a0e
Gaetan Sorin <gaetan.sorin@gmail.com>
2017-05-12 10:54:54 +0200

    Début IA

--- src/Player/IA.java
+++ src/Player/IA.java
@@ -1,7 +1,12 @@
 package Player;
 
+import Board.Couple;
 import Cards.HandPlayer;
 import Cards.PlayerAttribute;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.abs;
 
 /**
  * Created by thespygeek on 11/05/17.
@@ -32,6 +37,41 @@ public class IA extends Player{
         this.goldPoints = 0;
         attributeCards = new PlayerAttribute();
         this.playableCards = new HandPlayer();
+    }
+
+    float max(float a, float b) {
+        if (a > b) return a;
+        return b;
+    }
+
+    // Premier jet. Ne prend pas en compte si un but a ou non de l'or.
+    // TODO
+    // À implémenter : Si on sait qu'un but a de l'or, faire les calculs en fonctions de lui
+    //                 Si on sait qu'un but n'a pas d'or, l'ignorer
+    public Couple choosePosition(ArrayList<Couple> p) {
+        int y;
+        float h, hMax = 0;
+        Couple bestCpl = p.get(0),
+               currentCpl;
+
+        // TODO : faire le calcul pour toutes les cartes de la main
+        for (int i = 0; i < p.size(); i++) {
+            currentCpl = p.get(i);
+            y = abs(8 - currentCpl.getY());
+
+            // Calcule de l'heuristique
+            // Soit p une position
+            // h(p) = max ( distance(p, but1), distance(p, but2), distance(p, but3) )
+            // avec distance(p, but) = |(but.x - p.x)| / |(but.y - p.y)|
+            h = max( max(abs(2 - currentCpl.getX()) / y, abs(-2 - currentCpl.getX()) / y), abs(0 - currentCpl.getX()) / y);
+
+            if (h > hMax) { // On garde le couple qui a la meilleure heuristique
+                hMax = h;
+                bestCpl = currentCpl;
+            }
+        }
+
+        return bestCpl;
     }
 
     @Override
