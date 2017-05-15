89e24cad54c82694efe2cad3c4ab13285e08c1d7
Gaetan Sorin <gaetan.sorin@gmail.com>
2017-05-15 10:07:44 +0200

    Modification de pour cycler parmis les cartes dans la main de l'IA

--- src/Player/IA.java
+++ src/Player/IA.java
@@ -1,11 +1,14 @@
 package Player;
 
 import Board.Couple;
+import Cards.Card;
+import Cards.GalleryCard;
 import Cards.HandPlayer;
 import Cards.PlayerAttribute;
 
 import java.util.ArrayList;
 
+import static Cards.Card.Card_t.gallery;
 import static java.lang.Math.abs;
 
 /**
@@ -45,29 +48,37 @@ public class IA extends Player{
     }
 
     // Premier jet. Ne prend pas en compte si un but a ou non de l'or.
-    // TODO
-    // À implémenter : Si on sait qu'un but a de l'or, faire les calculs en fonctions de lui
-    //                 Si on sait qu'un but n'a pas d'or, l'ignorer
-    public Couple choosePosition(ArrayList<Couple> p) {
+    // TODO : À implémenter : Si on sait qu'un but a de l'or, faire les calculs en fonctions de lui
+    // TODO :                 Si on sait qu'un but n'a pas d'or, l'ignorer
+    public Couple choosePosition() {
         int y;
         float h, hMax = 0;
-        Couple bestCpl = p.get(0),
+        Card currCard;
+        Couple bestCpl = new Couple(0, 0),
                currentCpl;
+        ArrayList<Couple> p;
 
         // TODO : faire le calcul pour toutes les cartes de la main
-        for (int i = 0; i < p.size(); i++) {
-            currentCpl = p.get(i);
-            y = abs(8 - currentCpl.getY());
+        for (int cardIdx = 0; cardIdx < nbCardHand(); cardIdx++) {
+            currCard = lookAtCard(cardIdx);
+            if (currCard.getType() == gallery) {
+                p = this.board.getPossiblePositions((GalleryCard) currCard);
+                bestCpl = p.get(0);
+                for (int i = 0; i < p.size(); i++) {
+                    currentCpl = p.get(i);
+                    y = abs(8 - currentCpl.getY());
 
-            // Calcule de l'heuristique
-            // Soit p une position
-            // h(p) = max ( distance(p, but1), distance(p, but2), distance(p, but3) )
-            // avec distance(p, but) = |(but.x - p.x)| / |(but.y - p.y)|
-            h = max( max(abs(2 - currentCpl.getX()) / y, abs(-2 - currentCpl.getX()) / y), abs(0 - currentCpl.getX()) / y);
+                    // Calcule de l'heuristique
+                    // Soit p une position
+                    // h(p) = max ( distance(p, but1), distance(p, but2), distance(p, but3) )
+                    // avec distance(p, but) = |(but.x - p.x)| / |(but.y - p.y)|
+                    h = max(max(abs(2 - currentCpl.getX()) / y, abs(-2 - currentCpl.getX()) / y), abs(0 - currentCpl.getX()) / y);
 
-            if (h > hMax) { // On garde le couple qui a la meilleure heuristique
-                hMax = h;
-                bestCpl = currentCpl;
+                    if (h > hMax) { // On garde le couple qui a la meilleure heuristique
+                        hMax = h;
+                        bestCpl = currentCpl;
+                    }
+                }
             }
         }
 
