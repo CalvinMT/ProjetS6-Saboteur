dd593790353bde2884ffef2ceeb43b45814605c4
Sanory <t.hibs@hotmail.fr>
2017-05-18 15:50:10 +0200

    Petites modifs

--- src/IHM/EndGame.java
+++ src/IHM/EndGame.java
@@ -1,3 +1,4 @@
+
 package IHM;
 
 import Cards.RoleCard;
--- src/IHM/PublicInfos.java
+++ src/IHM/PublicInfos.java
@@ -1,8 +1,31 @@
 package IHM;
 
-public class PublicInfos{
-  
-    }
-    
-        
-    
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class PublicInfos {
+
+    @FXML
+    private AnchorPane AnchorPanePublicInfos;
+
+    @FXML
+    private ImageView imageViewAvatar;
+
+    @FXML
+    private Text textPseudo;
+
+    @FXML
+    private Text textCartesRestantes;
+
+    @FXML
+    private ImageView imageViewStatusPickaxe;
+
+    @FXML
+    private ImageView imageViewStatusLanterne;
+
+    @FXML
+    private ImageView imageViewStatusWagon;
+
+}
