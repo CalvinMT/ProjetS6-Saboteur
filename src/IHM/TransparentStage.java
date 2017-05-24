c4b94d5700523a177de66e8d7d318e7b67f63191
Sanory <t.hibs@hotmail.fr>
2017-05-24 11:21:38 +0200

    Ajout boutton quitter calque aide

--- src/IHM/TransparentStage.java
+++ src/IHM/TransparentStage.java
@@ -1,7 +1,12 @@
 package IHM;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
@@ -17,11 +22,25 @@ public class TransparentStage extends Application {
         stage.initStyle(StageStyle.TRANSPARENT);
         ImageView imageview =new ImageView("ressources/calqueaide.png");
         AnchorPane pane = new AnchorPane();
-        pane.getChildren().add(imageview);
+        Button button = new Button();
+        button.setId("ButtonQuitterAide"); 
+        button.setGraphic(pane);
+        button.setGraphic(new ImageView("ressources/aideneg.png"));
+        button.setMaxSize(25.0, 25.0);
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                 ((Stage)button.getScene().getWindow()).close();
+                }
+             });
+        pane.getChildren().add(button);
+        //pane.getChildren().add(imageview);
         final Scene scene = new Scene(pane,1920, 1080);
         scene.setFill(null);
         stage.setScene(scene);
         stage.show();
+    }
+    
+    public void handlerQuitterAide() throws IOException {
     }
 
     public static void main(String[] args) {
