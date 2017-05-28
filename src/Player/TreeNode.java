37dabfbea5acb27835fa517375280d713a20bccf
Gaetan Sorin <gaetan.sorin@gmail.com>
2017-05-28 16:55:45 +0200

    Implementation evaluation config + ajout constructeur

--- src/Player/TreeNode.java
+++ src/Player/TreeNode.java
@@ -1,8 +1,12 @@
 package Player;
 
 import Board.Board;
+import Cards.GalleryCard;
 
 import java.util.ArrayList;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
 
 /**
  * Created by oloar on 26/05/2017.
@@ -16,7 +20,10 @@ public class TreeNode {
         this(new Board(), true, new ArrayList<>());
     }
 
-    public TreeNode(Board board,boolean isMax, ArrayList<TreeNode> next) {
+    public TreeNode(Board board, boolean isMax) {
+        this(board, isMax, new ArrayList<>());
+    }
+    public TreeNode(Board board, boolean isMax, ArrayList<TreeNode> next) {
         this.board = board;
         this.isMax = isMax;
         this.next = next;
@@ -47,37 +54,21 @@ public class TreeNode {
         return isMax;
     }
 
-    public int evaluate() {
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        // TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-        return 0;
+    public int getMinDistanceToGoals(GalleryCard c) {
+        return min(abs(this.board.getMineElement(1).card.getLine() - c.getLine()) + abs(this.board.getMineElement(1).card.getColumn() - c.getColumn()),
+               min(abs(this.board.getMineElement(2).card.getLine() - c.getLine()) + abs(this.board.getMineElement(2).card.getColumn() - c.getColumn()),
+                   abs(this.board.getMineElement(3).card.getLine() - c.getLine()) + abs(this.board.getMineElement(3).card.getColumn() - c.getColumn())));
+    }
+
+    public float evaluate() {
+        int i;
+        GalleryCard curr;
+        float value = 0;
+        for (i = 0; i < this.board.getMineSize(); i++) {
+            curr = this.board.getMineElement(i).card;
+            if (this.board.getAccessCardElement(curr.getCoord()).equals(curr)) // On ne considère que les cartes accessibles
+                value += getMinDistanceToGoals(curr);
+        }
+        return value / i; // Distance moyenne au but
     }
 }
